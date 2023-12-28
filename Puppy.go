7fc2843ff6a5b087ab392ddf76903a0a9e36adeb
man_crest <man.m@crestinfosystems.com>
2023-12-28 14:22:23 +0530

    dog package added

--- Puppy.go
+++ Puppy.go
@@ -1,9 +1,18 @@
 package Puppy
 
+import (
+	"github.com/Man-Crest/Dog"
+)
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "woof woof woof"
+
+}
+
+func Aware() string {
+	return Dog.Dog()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Man-Crest/Puppy-package
 
 go 1.21.5
+
+require github.com/Man-Crest/Dog v0.0.0-20231228084757-8227f6e3287a // indirect
